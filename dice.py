92b810892b1de9b37186f57caa70ca43410f4ad5
Rafael MURO <rafael.francisco.muro@gmail.com>
2024-03-12 16:15:02 +0100

    Tamère

---
+++ dice.py
@@ -0,0 +1,14 @@
+from random import randint
+
+
+class Dice:
+    def __init__(self, nb_faces) -> None:
+        self.nb_faces = nb_faces
+
+
+    def __str__(self) -> str:
+        return f"Dice {self.nb_faces}"
+    
+    def roll(self) -> int:
+        return randint(1, self.nb_faces)
+    
