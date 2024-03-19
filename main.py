b1d559a477a30b8f121343b9093e17bc1b3686f4
Gashrod1 <101563672+Gashrod1@users.noreply.github.com>
2024-03-19 13:08:02 +0100

    Salut salut

--- main.py
+++ main.py
@@ -2,17 +2,10 @@ from game import *
 import time
 game = Game()
 
-"""game.start()
+game.start()
 
-game.hub()"""
+game.hub()
 
-
-r_loot1 = game.generate_loot()
-r_loot2 = game.generate_loot()
-r_loot3 = game.generate_loot()
-r_loot4 = game.generate_loot()
-
-print(r_loot1.name,r_loot2.name,r_loot3.name,r_loot4.name)
 
 
 
