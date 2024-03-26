933959b8c87774b5dfdf07c3a2bf1389458786ad
KaRZa13 <rafael.francisco.muro@gmail.com>
2024-03-26 19:26:36 +0100

    Dernier commit avant rendu

--- dice.py
+++ dice.py
@@ -4,7 +4,7 @@ from random import randint
 class Dice:
     def __init__(self, nb_faces) -> None:
         self.nb_faces = nb_faces
-
+        
 
     def __str__(self) -> str:
         return f"Dice {self.nb_faces}"
